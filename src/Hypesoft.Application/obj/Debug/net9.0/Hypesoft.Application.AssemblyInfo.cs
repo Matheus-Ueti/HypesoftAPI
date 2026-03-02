0c3c14226c63f0ea3d36dea91a9ac54c597c43ff
Matheus-Ueti <matheusueti@gmail.com>
2026-03-02 17:33:15 -0300

    feat: add docker-compose with MongoDB and Keycloak

--- src/Hypesoft.Application/obj/Debug/net9.0/Hypesoft.Application.AssemblyInfo.cs
+++ src/Hypesoft.Application/obj/Debug/net9.0/Hypesoft.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hypesoft.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd561478d1d53be3b8dfedf8b9e4fd79a125566c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18c252883414a38d9e4c16e06e0c0266fb9f149")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hypesoft.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hypesoft.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
